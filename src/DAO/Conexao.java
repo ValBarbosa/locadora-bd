9e21cacdb31358436e58d8b3287a0d27bb2d8e5e
valeria b <valmariasilva53@gmail.com>
2018-10-24 19:28:30 -0300

    bibliotecas e Conexao e ExecuteSQL

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -5,3 +5,35 @@
  */
 package DAO;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+
+public class Conexao {
+    public static Connection AbrirConexao(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/locadora";
+            con = (Connection) DriverManager.getConnection(url, "root", "123");
+        } catch (Exception e) {
+    JOptionPane.showMessageDialog(null, "erro na conexao com o banco",
+            "video locadora", JOptionPane.ERROR_MESSAGE);
+            e.getMessage();
+        
+        }
+        return con;
+    }
+    public static void FecharConexao(Connection con){
+        try {
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+}
+
+
